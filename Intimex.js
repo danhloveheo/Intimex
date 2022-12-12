82ac55ea2c680992acf1775ca291f391309c2fea
Danh <Danhlt2@fpt.com.vn>
2022-12-12 17:02:50 +0700

    a
    a

--- Intimex.js
+++ Intimex.js
@@ -1,26 +1,24 @@
 ﻿$(document).ready(function () {
     getData();
     getListSheets();
-    setTimeout(function () {  createControl()
-        , 2000; }, 4000);
-
+    
    
-
-
 });
 var listSheets = [];
+var listall = [];
 
 
 function getData() {
 
     $.ajax({
-        url: 'https://script.google.com/macros/s/AKfycby3gPLc-c9DilLv2Tf2bt9phceRR61jyAqxf5tNJNFyBjcYwk-AwlExMCeCJH3t5Y-67Q/exec',
+        url: 'https://script.google.com/macros/s/AKfycbxtcCEl9zZ9QYUNho0XkEDj14vn8JmgRGgOgYxiJqXbub1Yt6Bk2yqpCzDkLcBU9sm25Q/exec',
         success: function (result) {
 
             listall = result;
             console.log("--");
             console.log(listall);
             console.log("--");
+         
         },
         error: function () {
             console.log("err");
@@ -38,6 +36,7 @@ function getListSheets() {
             console.log("--");
             console.log(listSheets);
             console.log("--");
+            createControl();
         },
         error: function () {
             console.log("err");
@@ -65,39 +64,23 @@ function createControl() {
     });
   
    
-    var datafStatusAssign = [
-{ text: 'Đã PC', value: '1' },
-{ text: 'Chưa PC', value: '2' },
-
-    ];
-    $('#fStatusAssign').kendoDropDownList({
-        dataTextField: 'text',
-        dataValueField: 'value',
-        dataSource: datafStatusAssign,
-
-    });
 
 
 
 
-   
-
-
-
-
-    $('#btnAssign').kendoButton({
-        click: function (e) {
-            LoadAssign();
-        }
-    });
     $('#btnViewReport').kendoButton({
         click: function (e) {
-            LoadAssignReport();
+            LoadReport();
+        }
+    });
+    $('#btnInsertReport').kendoButton({
+        click: function (e) {
+            window.open("https://docs.google.com/spreadsheets/d/13BWkyA7_qEd7VdTa3vGtj1T-ZAm66T8D2-ek-T6GbMQ/edit?usp=sharing", "PopUpGet","toolbars=no,scrollbars=yes,status=yes,resizable=no");
         }
     });
 
 
-    $('#gridAssign').kendoGrid({
+    $('#gridReport').kendoGrid({
         excel: {
             allPages: true,
             filterable: true
@@ -133,7 +116,7 @@ function createControl() {
 
     });
 
-    $('#gridReport').kendoGrid({
+    $('#gridDetail').kendoGrid({
         excel: {
             allPages: true,
             filterable: true
@@ -173,67 +156,7 @@ function createControl() {
 
     });
 
-    $('#btnUpdateAssignRoute').on('click', UpdateAssignRoute);
-    function UpdateAssignRoute() {
-        var r = confirm('Bạn có muốn cập nhật?');
-        if (r == false) {
-            return;
-        } else {
-
-            var ArrUpdate = [];
-
-            var checkErr = 0;
-
-
-            var fi = getFilter();
-            var displayedData = $('#gridAssign').data().kendoGrid.dataSource.view();
-            var length = displayedData.length;
-            console.log(displayedData);
-            for (var i = 0; i < length; i++) {
-                var currentDataItem = displayedData[i];
-
-                var AccountNVVP = $('[id=dropDownTemplateNVVP][name=' + currentDataItem.STT + ']').val();
-                if (currentDataItem.StaffID == null) var StaffID = "-1"
-                if (AccountNVVP != StaffID
-           ) {
-                    ArrUpdate.push({
-
-                        SubParentDescVN: currentDataItem.SubParentDescVN,
-                        LocationID: currentDataItem.LocationID,
-                        District: currentDataItem.DistrictID,
-                        Ward: currentDataItem.WardID,
-                        Street: currentDataItem.StreetID,
-                        StaffID: AccountNVVP
-                    });
-                }
-            }
-            if (ArrUpdate.length > 0) {
-
-                console.log(JSON.stringify(ArrUpdate));
-
-                //  kendo.ui.progress($('#gridViewKPI'), true);
-                $.ajax({
-                    type: 'POST',
-                    contentType: 'application/json',
-
-                    url: '/Contract/StaffLocationAssign/InsertCaseStaffRoute',
-                    data: JSON.stringify(ArrUpdate),
-                    success: function (result) {
-                        console.log(result.query);
-                        alert("Cập nhật thành công")
-                    },
-                    error: function (error) {
-                        console.log(error);
-                        alert("Phân công thất bại! Vui lòng liên hệ ISC");
-                    }
-                });
-
-
-
-            }
-
-        }
-    }
+   
     function LoadAssignReport() {
         var d = getFilter();
         var tabstrip = $('#tabstrip').data('kendoTabStrip');
@@ -283,101 +206,42 @@ function createControl() {
 
     }
 
-    function LoadAssign() {
+    function LoadReport() {
         var d = getFilter();
+        var listconSheets = listall;
+        if(d.fSheets != '')
+        {
+            
+        var listconSheets = listall.filter(function (item) {
+            return item.Sheet == d.fSheets ;
+        });
+        }
         var tabstrip = $('#tabstrip').data('kendoTabStrip');
         tabstrip.select(1);
         tabstrip.enable(tabstrip.select(), true);
-        var grid = $('#gridAssign').data("kendoGrid");
+        var grid = $('#gridReport').data("kendoGrid");
         var options = grid.options;
-        options.columns = colViewAssign();
-        $.ajax({
-            url: '/Contract/StaffLocationAssign/LoadViewAssign',
-            data: {
-
-                fSub: d.fSub,
-                fLocation: d.fLocation,
-                fDistrict: d.fDistrict,
-                fWard: d.fWard,
-                fStreet: d.fStreet,
-                fStatusAssign: d.fStatusAssign,
-                fStaff: d.fStaff
-
-            },
-
-            beforeSend: function () {
-                $('.ViewLoader1').css('display', 'block');
-                kendo.ui.progress($('#gridAssign'), true);
-            },
-            complete: function () {
-                $('.ViewLoader1').css('display', 'none');
-                kendo.ui.progress($('#gridAssign'), false);
-            },
-            success: function (data) {
-                console.log(data);
-                var dataSource = new kendo.data.DataSource({
-                    data: data.ds,
-                    pageSize: 100,
-                });
-                options.dataSource = dataSource;
-                $('#gridAssign').empty().kendoGrid(options);
-
-
-            },
-            error: function (error) {
-                alert('errror: ' + error);
-            }
+        options.columns = colView();
+        var dataSource = new kendo.data.DataSource({
+            data: listconSheets,
+            pageSize: 100,
         });
+        options.dataSource = dataSource;
+        $('#gridReport').empty().kendoGrid(options);
 
 
     }
-    function colViewAssign() {
+    function colView() {
         var d = getFilter();
         var columns =
              [
              {
-                 field: "STT", title: "STT", width: "20px",
+                 field: "Name", title: "Name", width: "20px",
                  headerAttributes:
                  { style: "text-align: center; font-weight: bold;white-space: normal" },
                  attributes: { style: "text-align:center;" }
              },
-             {
-                 field: "SubParentDescVN", title: "Vùng", width: "50px",
-                 headerAttributes:
-                 { style: "text-align: center; font-weight: bold;white-space: normal" },
-                 attributes: { style: "text-align:center;" }
-             },
-             {
-                 field: "DescriptionVN", title: "Tỉnh thành", width: "50px",
-                 headerAttributes:
-                 { style: "text-align: center; font-weight: bold;white-space: normal" },
-                 attributes: { style: "text-align:center;" }
-             },
-             {
-                 field: "District", title: "Quận(Huyện)", width: "50px",
-                 headerAttributes:
-                 { style: "text-align: center; font-weight: bold;white-space: normal" },
-                 attributes: { style: "text-align:center;" }
-             },
-             {
-                 field: "Ward", title: "Phường(Xã)", width: "50px",
-                 headerAttributes:
-                 { style: "text-align: center; font-weight: bold;white-space: normal" },
-                 attributes: { style: "text-align:center;" }
-             },
-              {
-                  field: "Street", title: "Tuyến đường", width: "50px",
-                  headerAttributes:
-                  { style: "text-align: center; font-weight: bold;white-space: normal" },
-                  attributes: { style: "text-align:center;" }
-              },
-              {
-                  field: "Name", title: "Acc phụ trách", width: "70px",
-                  headerAttributes:
-                  { style: "text-align: center; font-weight: bold;white-space: normal" },
-                  template: '<input id="dropDownTemplateNVVP" name = "#=STT#" style="width: 150px;"/><br>',
-                  attributes: { style: "text-align:center;" }
-              },
+             
              ]
         return columns;
     }
@@ -441,22 +305,16 @@ function createControl() {
 
 
 function getFilter() {
-    var fSub = $("#fSub").val();
-    var fLocation = $("#fLocation").val();
-    var fDistrict = $("#fDistrict").val();
-    var fWard = $("#fWard").val();
-    var fStreet = $("#fStreet").val();
-    var fStatusAssign = $("#fStatusAssign").val();
-    var fStaff = $("#fStaff").val();
+    var fSheets = $("#fSheets").val();
+    var fContract = $("#fContract").val();
+    var fBooking = $("#fBooking").val();
+  
 
     var filter = {
-        fSub: fSub,
-        fLocation: fLocation,
-        fDistrict: fDistrict,
-        fWard: fWard,
-        fStreet: fStreet,
-        fStatusAssign: fStatusAssign,
-        fStaff: fStaff
+        fSheets: fSheets,
+        fContract: fContract,
+        fBooking: fBooking
+       
 
     }
     return filter;
